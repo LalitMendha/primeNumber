a15bd6a95fa17c04ca23c670441f43cefcd7848f
LalitMendha <lalitmendha102@gmail.com>
2020-12-11 00:51:09 +0530

    feat: calculatePrime
    Added calculate.js file which checks whether the entered number is prime or not.

---
+++ calculate.js
@@ -0,0 +1,31 @@
+// Calculate whether the given number is prime number or not.
+
+
+var btnCalculate = document.querySelector("#btn-calculate");
+btnCalculate.addEventListener("click",clickHandler);
+
+function clickHandler() {
+    var inputNumber = document.querySelector("#numberInput").value;
+    var outputText = document.querySelector("#outputText");
+    var prime = checkPrime(inputNumber);
+    if(prime === "X"){
+        outputText.innerText = "The Number is a prime number";
+    }else {
+        outputText.innerText = "The Number is not a prime number";
+    }
+}
+
+function checkPrime(inputNumber) {
+    if(inputNumber === "2"){
+        return "X";
+    }
+
+    for(var i=2; i<(inputNumber-1); i++) {
+        if(inputNumber % 2 === 0){
+            return "";
+        }
+    }
+    return "X";
+}
+
+
